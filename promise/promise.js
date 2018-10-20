c845997748436a17e092025953134cc52d7ac383
tianguangrong <tiangr@i2finance.net>
2018-10-20 11:00:35 +0800

    promise-捕获异常

--- promise/promise.js
+++ promise/promise.js
@@ -26,17 +26,18 @@ var loadImg(src){
 var src = "https://www.baidu.com/img/bd_logo1.png";
 var resolve = loadImg(src);
 
-resolve.then(function(){
+resolve.then(function(arg){
 	//加载成功后的代码扩展
-},function(){
+	return arg //否则下面的.then将不会接收到arg
+},function(arg){
 	//加载失败后的代码扩展
-}).then(function(){
+}).then(function(arg){
 	//加载成功后的代码扩展
-},function(){
+},function(arg){
 	//加载失败后的代码扩展
-}).then(function(){
+}).then(function(arg){
 	//加载成功后的代码扩展
-},function(){
+},function(arg){
 	//加载失败后的代码扩展
 })
 
@@ -44,3 +45,27 @@ resolve.then(function(){
 //增加了代码的扩展代码的开放性和修改代码的封闭性;
 //具体体现:
 //jQuery 1.5 版本前后 对于ajax 的封装的改变;
+
+
+//如何捕获异常?
+//规定.then只传一个参数,最后于用catch进行异常捕获;
+resolve.then(function(arg){
+	//加载成功后的代码扩展
+	return arg //否则下面的.then将不会接收到arg
+}).then(function(arg){
+	//加载成功后的代码扩展
+}).then(function(arg){
+	//加载成功后的代码扩展
+}).catch(function(ex){
+	//统一进行异常捕获
+	console.log(ex)
+})
+
+
+
+
+
+
+
+
+
